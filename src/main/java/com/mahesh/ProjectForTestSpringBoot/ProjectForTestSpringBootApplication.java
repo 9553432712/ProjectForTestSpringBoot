c453c4698673bb55218ed4478020de3113dc62c9
mahesh <mahesh.rvrjc@gmail.com>
2018-12-20 16:39:27 +0530

    added initial project

---
+++ src/main/java/com/mahesh/ProjectForTestSpringBoot/ProjectForTestSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.mahesh.ProjectForTestSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForTestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectForTestSpringBootApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/mahesh/ProjectForTestSpringBoot/ProjectForTestSpringBootApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mahesh.ProjectForTestSpringBoot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectForTestSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
